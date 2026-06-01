386b35853188c86f864c2845c4a5d661098b3ff2
JOAO AUGUSTO TONIAL LANER <01035589001@senacrs.edu.br>
2026-06-01 17:29:54 -0300

    Add new exercises

---
+++ proj/examples.py
@@ -0,0 +1,15 @@
+################# Exercícios (1ª aula) #################
+
+def calcula_area_triangulo(base: float, altura: float):
+    area = (base * altura) / 2
+    print(f"A área do triângulo é: {area}")
+
+def converte_temperatura_fahrenreit(graus_celsius: float):
+    temperatura = (graus_celsius * (9/5)) + 32
+    print(f"A temperatura em fahrenreit é: {temperatura}")
+
+def converte_em_dolares(reais: float):
+    dolares = reais / 5.04
+    print(f"O valor em dólares é: {dolares}")
+
+
